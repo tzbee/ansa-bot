9b1afb7701dc139262978859d44db91c38c24775
touzbi <tolbimine@gmail.com>
2014-02-18 07:18:22 +0200

    Added Tag Grabber & JSoup Tag Grabber

---
+++ src/com/touzbi/ansa/contentgrabber/taggrabber/JSoupTagGrabber.java
@@ -0,0 +1,12 @@
+package com.touzbi.ansa.contentgrabber.taggrabber;
+
+import com.touzbi.ansa.htmlelement.HTMLElement;
+
+public class JSoupTagGrabber implements TagGrabber {
+
+	@Override
+	public HTMLElement getTag(String query) {
+		// TODO JSoup Tag grabber
+		return null;
+	}
+}
---
+++ src/com/touzbi/ansa/contentgrabber/taggrabber/TagGrabber.java
@@ -0,0 +1,7 @@
+package com.touzbi.ansa.contentgrabber.taggrabber;
+
+import com.touzbi.ansa.htmlelement.HTMLElement;
+
+public interface TagGrabber {
+	HTMLElement getTag(String query);
+}
