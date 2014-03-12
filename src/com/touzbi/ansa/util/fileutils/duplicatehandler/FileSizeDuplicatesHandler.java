b924b1f5fe3fa368a7794f3898ee9d461fb8a805
touzbi <tolbimine@gmail.com>
2014-03-12 02:53:26 +0200

    Refactoring Deleted unused stuff

--- src/com/touzbi/ansa/util/fileutils/duplicatehandler/FileSizeDuplicatesHandler.java
+++
@@ -1,9 +0,0 @@
-package com.touzbi.ansa.util.fileutils.duplicatehandler;
-
-import com.touzbi.ansa.util.fileutils.filecomparator.SizeFileComparator;
-
-public class FileSizeDuplicatesHandler extends FileDuplicatesHandler {
-	public FileSizeDuplicatesHandler() {
-		super(new SizeFileComparator());
-	}
-}
